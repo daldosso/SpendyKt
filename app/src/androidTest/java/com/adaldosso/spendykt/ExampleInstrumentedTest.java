1f7f476496a75451d890368daaab5abd208c68de
Alberto Dal Dosso <daldosso@gmail.com>
2016-12-05 17:39:07 +0100

    Added login activity

--- app/src/androidTest/java/com/adaldosso/spendykt/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/adaldosso/spendykt/ExampleInstrumentedTest.java
@@ -12,8 +12,8 @@ import static org.junit.Assert.*;
 /**
  * Instrumentation test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+ * @see <a href="http://d.android.com/tools/testing">Testingdocumentation</a>
+        */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
--- app/src/main/java/com/adaldosso/spendykt/MainActivity.kt
+++ app/src/main/java/com/adaldosso/spendykt/MainActivity.kt
@@ -2,11 +2,38 @@ package com.adaldosso.spendykt
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addDrawer()
+    }
+
+    private fun addDrawer() {
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setTitle(R.string.drawer_item_custom_container_drawer)
+
+        val item1 = PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home)
+        val item2 = SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_settings)
+
+        val result = DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        DividerDrawerItem(),
+                        item2,
+                        SecondaryDrawerItem().withName(R.string.drawer_item_settings)
+                )
+                .withOnDrawerItemClickListener { view, position, drawerItem -> true }
+                .build()
     }
 }
